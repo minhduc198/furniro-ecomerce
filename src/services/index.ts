5ab9617c6ef44e482ca4bb5ff5a2ec0600f67a13
nguyenminhduc1982005@gmail.com <nguyenminhduc1982005@gmail.com>
2025-01-22 20:33:06 +0700

    Chore: change base api url

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 import { ApiResponse, IDataProduct } from '../types'
 
-const baseUrl = 'http://localhost:4000'
+const baseUrl = 'https://furniro-api-j4bw.onrender.com'
 
 export async function getListProduct(page: number, limit: number): Promise<ApiResponse<IDataProduct>> {
   const response = await fetch(`${baseUrl}/product?page=${page}&limit=${limit}`)
